fbcf96e3b199bc4fc0b286dd3a60206586768262
John Cho <johnycho16@gmail.com>
2019-03-07 03:37:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,7 @@ programmer_hash[:yukihiro_matsumoto] = {}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
 
-
+puts programmer_hash
 end
 
 
